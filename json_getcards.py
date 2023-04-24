e8f252f477556a7b8976cb3d147559032db8ca18
Josef Soza-Foias <91845564+SozaFoiasJosef@users.noreply.github.com>
2023-04-24 14:24:58 -0400

    Add files via upload

--- json_getcards.py
+++ json_getcards.py
@@ -6,7 +6,7 @@ with open("filtered_cards.json", encoding="utf-8") as f:
     json_data = json.load(f)
 
 #filtered_data = [item for item in json_data if item["set"] == "mh2"]
-#filtered_data = [item for item in json_data if "image_uris" in item and "normal" in item["image_uris"]]
+filtered_data = [item for item in json_data if "image_uris" in item and "normal" in item["image_uris"]]
 normal_images = [item["image_uris"]["normal"] for item in json_data if "image_uris" in item and "normal" in item["image_uris"]]
 
 
@@ -14,9 +14,3 @@ with open("filtered_cards1.json", "w") as f:
     json.dump(normal_images, f, indent=2)
 
 #print(json.dumps(filtered_data, indent=2))
-
-for i, url in enumerate(normal_images):
-    response = requests.get(url)
-    filename = f"image{i}.jpg"
-    with open(filename, "wb") as f:
-        f.write(response.content)
