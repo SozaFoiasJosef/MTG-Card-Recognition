9921b5b6118eed95c42edbe1b031130e07006f02
Josef Soza-Foias <91845564+SozaFoiasJosef@users.noreply.github.com>
2023-04-24 19:59:44 -0400

    Add files via upload

--- json_getcards.py
+++ json_getcards.py
@@ -6,13 +6,14 @@ with open("filtered_cards.json", encoding="utf-8") as f:
     json_data = json.load(f)
 
 filtered_data = [item for item in json_data if item["set"] == "mh2"]
-normal_images = [item["image_uris"]["normal"] for item in filtered_data if "image_uris" in item and "normal" in item["image_uris"]]
+normal_images = [item["image_uris"]["art_crop"] for item in filtered_data if "image_uris" in item and "art_crop" in item["image_uris"]]
 
 #filtered_data = [item for item in json_data if "image_uris" in item and "normal" in item["image_uris"]]
 #normal_images = [item["image_uris"]["normal"] for item in json_data if "image_uris" in item and "normal" in item["image_uris"]]
+#normal_images = [item["image_uris"]["art_crop"] for item in json_data if "image_uris" in item and "art_crop" in item["image_uris"]]
 
 
-with open("mh2set.json", "w") as f:
+with open("mh2set_artcrop.json", "w") as f:
     json.dump(normal_images, f, indent=2)
 
 #print(json.dumps(filtered_data, indent=2))
